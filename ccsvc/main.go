450cfccc63a7376bfe9aa6d4f52621c561ed1df2
Aung Maw <aungmawjj12@gmail.com>
2021-10-22 07:13:52 +0800

    pub sub

--- ccsvc/main.go
+++ ccsvc/main.go
@@ -50,40 +50,6 @@ func fetchAuctionResult(c echo.Context) error {
 	return c.JSON(http.StatusOK, result)
 }
 
-// func fetchAuctionResult(c echo.Context) error {
-// 	var req AuctionResultRequest
-// 	err := c.Bind(&req)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	eth, err := ethclient.Dial("http://localhost:8545")
-// 	if err != nil {
-// 		return err
-// 	}
-// 	cc, err := contract.NewAuction(common.BytesToAddress(req.Address), eth)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	var result AuctionResult
-// 	result.Ended, err = cc.Ended(&bind.CallOpts{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	highestBid, err := cc.HighestBid(&bind.CallOpts{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	result.HighestBid = highestBid.Int64()
-
-// 	highestBidder, err := cc.HighestBidder(&bind.CallOpts{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	result.HighestBidder = highestBidder.Bytes()
-
-// 	return c.JSON(http.StatusOK, result)
-// }
-
 func onReceiveAuctionResult(result *AuctionResult) {
 	log.Printf("Received auction result, %s\n", hex.EncodeToString(result.Address))
 	auctionResults[string(result.Address)] = result
